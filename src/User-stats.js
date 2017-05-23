7c2f000e5c614359e738bd4abeb12f01f6e2c8e9
nicky evers <livelyevers@gmail.com>
2017-05-23 12:11:33 -0700

    basic box styling

--- src/User-stats.js
+++ src/User-stats.js
@@ -5,7 +5,7 @@ function UserStats({ user }) {
   return (
     <div className="user-block">
       <p>Player: {user.name}</p>
-      <p>Items: {user.items}</p>
+      <p>Items: {user.items.map(item => `${item.name}  `)}   </p>
       <p>Teabuzz: {user.teaBuzz} / 100</p>
     </div>
   );
