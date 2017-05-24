5c60049c3d6a6ce8c397f1cbf01d3b39ba1581ef
nicky evers <livelyevers@gmail.com>
2017-05-24 14:07:08 -0700

    fixed typo

--- src/scenes/08-villain-battle.js
+++ src/scenes/08-villain-battle.js
@@ -4,7 +4,7 @@ export default {
   backgroundUrl: battleImg,
   noncommital: true,
   headerText: 'Fight!',
-  bodyText: 'Oh no! The villain that stole you tea was waiting for you in the tree, teabuzzed beyond sanity from all your tea. Do you have what it takes to defeat him?',
+  bodyText: 'Oh no! The villain that stole your tea was waiting for you in the tree, teabuzzed beyond sanity from all your tea. Do you have what it takes to defeat him?',
   buttonText2: 'Swing sword',
   items: [],
   villain: { name: 'Tea Stealer', health: 100, category: 'tea' }
