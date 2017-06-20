075583979e2fdbe9e3d5accc3472dae2c9126a1a
Ivy Patton <lilaivy@gmail.com>
2017-06-20 13:29:08 -0700

    added reset buttons

--- hawaii-adventure/src/Beach.js
+++ hawaii-adventure/src/Beach.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import beach from './images/beach.jpg';
+import ChangeSceneButton from './ChangeSceneButton';
+import UserSignin from './UserSignin'
 
-export default function Beach() {
+export default function Beach({changeScene, Scene}) {
     return (
         <div classname="beach">
             <img src={beach} className="beachImage" alt='beach' />
             <h2>You made it to the beach! YOU WIN!!!</h2>
+            <ChangeSceneButton
+         changeScene={changeScene}
+         Scene={UserSignin}
+         buttonMessage="PLAY AGAIN!!"/>
         </div>
     );
 }
--- hawaii-adventure/src/Dead.js
+++ hawaii-adventure/src/Dead.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import dead from './images/dead.png';
+import ChangeSceneButton from './ChangeSceneButton';
+import UserSignin from './UserSignin'
 
-export default function Dead() {
+export default function Dead( {changeScene, Scene}) {
   return(
   <div className="dead">
     <h2>Oh no! You died of boredom at the Luau! GAME OVER</h2>
     <img src={dead} className="deadImage" alt='dead' />
+             <ChangeSceneButton
+         changeScene={changeScene}
+         Scene={UserSignin}
+         buttonMessage="PLAY AGAIN!!"/>
   </div>
   );
 }
