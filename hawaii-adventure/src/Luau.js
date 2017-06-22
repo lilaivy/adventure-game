7b5158e8b2d5b7d6a5869315b546a82a0f660222
Ivy Patton <lilaivy@gmail.com>
2017-06-21 23:37:08 -0700

    reformatted Luau.js

--- hawaii-adventure/src/Luau.js
+++ hawaii-adventure/src/Luau.js
@@ -5,27 +5,27 @@ import Drunk from './Drunk'
 import ChangeSceneButton from './ChangeSceneButton';
 import luau from './images/luau.jpg'
 
-export default function Luau ({changeScene, Scene, name}) {
-return(
-<div className="Luau">
-<h2>Oh no {name}! You've encountered a touristy Luau at the resort. Do you throw on a grass skirt and join in on the hula action, head back to the bar, or go surfing with your new sufer homie?  </h2>
- <img src={luau} className="luau-image" alt='luau' />
-<ChangeSceneButton
-changeScene={changeScene}
-Scene={Dead}
-buttonMessage="Stay at The Luau"
-/>
-<ChangeSceneButton
-changeScene={changeScene}
-Scene={Drunk}
-buttonMessage="Go To Bar"
-/>
+export default function Luau({ changeScene, Scene, name }) {
+    return (
+        <div className="Luau">
+            <h2>Oh no {name}! You've encountered a touristy Luau at the resort. Do you throw on a grass skirt and join in on the hula action, head back to the bar, or go surfing with your new sufer homie?  </h2>
+            <img src={luau} className="luau-image" alt='luau' />
+            <ChangeSceneButton
+                changeScene={changeScene}
+                Scene={Dead}
+                buttonMessage="Stay at The Luau"
+            />
+            <ChangeSceneButton
+                changeScene={changeScene}
+                Scene={Drunk}
+                buttonMessage="Go To Bar"
+            />
 
-<ChangeSceneButton
-changeScene={changeScene}
-Scene={Beach}
-buttonMessage="Bail on Luau"
-/>
-</div>
-);
+            <ChangeSceneButton
+                changeScene={changeScene}
+                Scene={Beach}
+                buttonMessage="Bail on Luau"
+            />
+        </div>
+    );
 }
