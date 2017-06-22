81a15b8a2ce52b27a1f847a339b5abef73c88d1a
Ivy Patton <lilaivy@gmail.com>
2017-06-21 23:45:08 -0700

    deleted future components

--- hawaii-adventure/src/futureComponents.js
+++
@@ -1,7 +0,0 @@
-
-
-        /*<div className="Luau">
-          <h2>Oh no {name}! You've encountered a touristy Luau that is blocking your beach access! Throw on a grass skirt and hula your way through the crowd. </h2>
-          <img src={luau} className="luau" alt='luau' />
-          <ChangeSceneButton />
-        </div>*/
